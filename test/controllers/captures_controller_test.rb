b0b052949c8b9f28f025fa4c990461e207907688
Martin Berggren <mb.berggren@gmail.com>
2026-08-09 22:15:56 +0200

    Move manual capture creation into navigation

--- test/controllers/captures_controller_test.rb
+++ test/controllers/captures_controller_test.rb
@@ -21,10 +21,13 @@ class CapturesControllerTest < ActionDispatch::IntegrationTest
     assert_select "input[type='file']", count: 0
     assert_select "input[name='capture[collection_ids][]']", count: 0
     assert_select "input[name='capture[tag_ids][]']", count: 0
-    assert_select "a[href=?]", new_capture_path, text: "Add manually"
+    assert_select "form.quick-capture-form a[href=?]", new_capture_path, count: 0
     assert_select "a.application-brand[href=?]", root_path, text: "Infinity"
     assert_select "button.menu-toggle[popovertarget='application-menu']"
     assert_select "nav#application-menu[popover='auto'] a[href=?]", collections_path, text: "Collections"
+    assert_select "nav#application-menu[popover='auto'] a.menu-create[href=?]", new_capture_path
+    assert_select "a.menu-create .menu-create-icon", "+"
+    assert_select "a.menu-create span:not(.menu-create-icon)", "Add manually"
     assert_select "a", text: captures(:link).title
     assert_select "a", text: captures(:note).note, count: 0
   end
