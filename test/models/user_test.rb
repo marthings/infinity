275d3a4c0b02f9713a622f879077f95b08822e8a
Martin Berggren <mb.berggren@gmail.com>
2026-08-09 13:54:20 +0200

    Add public signup (#13)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,4 +5,11 @@ class UserTest < ActiveSupport::TestCase
     user = User.new(email_address: " DOWNCASED@EXAMPLE.COM ")
     assert_equal("downcased@example.com", user.email_address)
   end
+
+  test "requires a valid email_address" do
+    user = User.new(email_address: "invalid", password: "password", password_confirmation: "password")
+
+    assert_not user.valid?
+    assert_predicate user.errors[:email_address], :any?
+  end
 end
--- test/system/authentication_test.rb
+++ test/system/authentication_test.rb
@@ -1,12 +1,13 @@
 require "application_system_test_case"
 
 class AuthenticationTest < ApplicationSystemTestCase
-  test "signing in" do
-    visit new_session_path
+  test "signing up" do
+    visit new_registration_path
 
-    fill_in :email_address, with: users(:one).email_address
-    fill_in :password, with: "password"
-    click_on "Sign in"
+    fill_in "Email address", with: "new@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Confirm password", with: "password"
+    click_on "Create account"
 
     assert_current_path root_path
     assert_text "Infinity"
