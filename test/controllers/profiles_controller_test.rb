beef055b4f40a18f15d3d21d837ad364c0b183bb
Martin Berggren <mb.berggren@gmail.com>
2026-08-09 16:59:51 +0200

    Serve the application stylesheet

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -10,6 +10,7 @@ class ProfilesControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :success
     assert_select "meta[name=viewport][content='width=device-width,initial-scale=1,viewport-fit=cover']"
+    assert_select "link[rel=stylesheet][href*='application']"
     assert_select "h1", "Profile"
     assert_select "input[value=?]", users(:one).email_address
     assert_select "[data-native-form]"
